6a3d236741c8fc69960d111239cbbb1f1d919cd3
Uttam Bhagaur <uttambhagaur@gmail.com>
2024-02-01 23:11:23 +0530

    add jjwt dependency

---
+++ src/main/java/com/springsecurity/config/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.springsecurity.config;
+
+import com.springsecurity.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    public JwtAuthenticationFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
+
+    }
+}
---
+++ src/main/java/com/springsecurity/services/JwtService.java
@@ -0,0 +1,10 @@
+package com.springsecurity.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+}
